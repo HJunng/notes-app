c7c07107ac1c2c7694d691bdb5a69d74ad3bb661
HJunng <guswjd1gh@gmail.com>
2023-09-21 12:42:40 +0900

    Create Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+
+interface IHeaderProps {
+}
+
+const Header: React.FunctionComponent<IHeaderProps> = (props) => {
+  return (
+    <Navbar expand='lg' className='bg-body-tertiary' fixed='top' bg='dark' variant='dark'>
+      <Container>
+          <Navbar.Brand href="#home">
+            
+            React Bootstrap
+          </Navbar.Brand>
+        </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
